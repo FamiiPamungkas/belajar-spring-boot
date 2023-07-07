8e3cd46b5a3298c0b976a3d44dcac1baca232004
Fahmi Pamungkas <fame1302@gmail.com>
2023-07-07 16:47:06 +0700

    add DTO's to typescript generator

--- model/model.d.ts
+++ model/model.d.ts
@@ -1,6 +1,48 @@
 /* tslint:disable */
 /* eslint-disable */
-// Generated using typescript-generator version 3.2.1263 on 2023-07-07 14:17:03.
+// Generated using typescript-generator version 3.2.1263 on 2023-07-07 16:44:58.
+
+export interface MenuDTO {
+    id: number;
+    authority: string;
+    name: string;
+    description: string;
+    link: string;
+    group: string;
+    showOnNav: boolean;
+    icon: string;
+    children: MenuDTO[];
+    authorities: string[];
+    seq: number;
+}
+
+export interface RoleDTO {
+    id: number;
+    name: string;
+    description: string;
+    menus: MenuDTO[];
+}
+
+export interface UserAuthDTO {
+    id: number;
+    firstname: string;
+    lastname: string;
+    birthdate: string;
+    username: string;
+    email: string;
+    roles: RoleDTO[];
+    treeMenus: MenuDTO[];
+}
+
+export interface UserDTO {
+    id: number;
+    firstname: string;
+    lastname: string;
+    birthdate: string;
+    username: string;
+    email: string;
+    roles: RoleDTO[];
+}
 
 export interface BaseEntity extends Serializable {
     id: number;
@@ -59,8 +101,8 @@ export interface UserDetails extends Serializable {
     enabled: boolean;
     password: string;
     username: string;
-    accountNonExpired: boolean;
     credentialsNonExpired: boolean;
+    accountNonExpired: boolean;
     authorities: GrantedAuthority[];
     accountNonLocked: boolean;
 }

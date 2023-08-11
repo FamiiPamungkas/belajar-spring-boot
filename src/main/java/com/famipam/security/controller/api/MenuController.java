7aee5f49708bb5edf2fab3ca05f57fc4d2ac5bd0
Fahmi Pamungkas <fame1302@gmail.com>
2023-08-11 15:16:13 +0700

    add : Menu Controller

--- src/main/java/com/famipam/security/controller/api/MenuController.java
+++ src/main/java/com/famipam/security/controller/api/MenuController.java
@@ -1,14 +1,14 @@
 package com.famipam.security.controller.api;
 
+import com.famipam.security.dto.MenuDTO;
 import com.famipam.security.dto.RoleDTO;
-import com.famipam.security.dto.user.SimpleUser;
-import com.famipam.security.entity.Role;
+import com.famipam.security.entity.Menu;
 import com.famipam.security.exception.NotFoundException;
+import com.famipam.security.mapper.MenuMapper;
 import com.famipam.security.mapper.RoleMapper;
-import com.famipam.security.mapper.SimpleUserMapper;
 import com.famipam.security.model.SimpleOption;
+import com.famipam.security.repository.MenuRepository;
 import com.famipam.security.repository.RoleRepository;
-import com.famipam.security.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -22,53 +22,53 @@ import java.util.stream.Collectors;
 
 @Validated
 @RestController
-@RequestMapping("/api/v1/roles")
+@RequestMapping("/api/v1/menus")
 @RequiredArgsConstructor
-public class RoleController extends BaseController{
+public class MenuController extends BaseController {
 
+    private final MenuRepository menuRepository;
     private final RoleRepository roleRepository;
-    private final UserService userService;
+    private final MenuMapper menuMapper = new MenuMapper();
     private final RoleMapper roleMapper = new RoleMapper();
-    private final SimpleUserMapper userMapper = new SimpleUserMapper();
 
     @GetMapping
-    public ResponseEntity<List<RoleDTO>> getRoleList() {
-        List<RoleDTO> users = roleRepository.findAll()
+    public ResponseEntity<List<MenuDTO>> getRoleList() {
+        List<MenuDTO> menus = menuRepository.findAll()
                 .stream()
-                .map(roleMapper)
+                .map(menuMapper)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(users);
+        return ResponseEntity.ok(menus);
     }
 
     @GetMapping("/options")
     public ResponseEntity<List<SimpleOption>> getRoleAsOptions() {
-        List<SimpleOption> roles = roleRepository.findAll()
+        List<SimpleOption> menus = menuRepository.findAll()
                 .stream()
-                .map(role -> new SimpleOption(
-                        String.valueOf(role.getId()),
-                        role.getName())
+                .map(menu -> new SimpleOption(
+                        String.valueOf(menu.getId()),
+                        menu.getName())
                 ).toList();
-        return ResponseEntity.ok(roles);
+        return ResponseEntity.ok(menus);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<RoleDTO> getRole(
+    public ResponseEntity<MenuDTO> getRole(
             @PathVariable long id
     ) {
-        Role role = roleRepository.findById(id).orElseThrow(() -> new NotFoundException("Role [" + id + "] not found."));
-        return ResponseEntity.ok(roleMapper.apply(role));
+        Menu role = menuRepository.findById(id).orElseThrow(() -> new NotFoundException("Menu [" + id + "] not found."));
+        return ResponseEntity.ok(menuMapper.apply(role));
     }
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<List<SimpleUser>> findRoleUsers(
+    @GetMapping("/roles/{id}")
+    public ResponseEntity<List<RoleDTO>> findRoleUsers(
             @PathVariable long id
     ) {
-        Role role = roleRepository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Role [" + id + "] not found."));
+        Menu menu = menuRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Menu [" + id + "] not found."));
         return ResponseEntity.ok(
-                userService.findByRole(role)
+                roleRepository.findByMenu(menu)
                         .stream()
-                        .map(userMapper)
+                        .map(roleMapper)
                         .toList()
         );
     }
--- src/main/java/com/famipam/security/repository/RoleRepository.java
+++ src/main/java/com/famipam/security/repository/RoleRepository.java
@@ -1,11 +1,14 @@
 package com.famipam.security.repository;
 
+import com.famipam.security.entity.Menu;
 import com.famipam.security.entity.Role;
-import com.famipam.security.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    @Query("SELECT DISTINCT r FROM Role r JOIN r.menus m WHERE m = :menu")
+    List<Role> findByMenu(Menu menu);
 }
